c7bb339ad07738eb0a8f6323a6d5699932a2b915
jk <joern.kreutel@beuth-hochschule.de>
2016-11-11 13:25:08 +0100

    add client to project!!!

---
+++ org.dieschnittstelle.jee.esa.ue.jws5/src/main/java/org/dieschnittstelle/jee/esa/ue/jws5/junit/ProductCRUDSOAPClient.java
@@ -0,0 +1,49 @@
+package org.dieschnittstelle.jee.esa.ue.jws5.junit;
+
+//import org.apache.log4j.Logger;
+//import org.dieschnittstelle.jee.esa.entities.erp.AbstractProduct;
+//import org.dieschnittstelle.jee.esa.entities.erp.IndividualisedProductItem;
+//import org.dieschnittstelle.jee.esa.jws.ProductCRUDSOAPService;
+//import org.dieschnittstelle.jee.esa.jws.ProductCRUDWebService;
+//
+//import java.util.List;
+
+public class ProductCRUDSOAPClient {
+
+//	private ProductCRUDWebService proxy;
+//
+//	protected static Logger logger = Logger.getLogger(ProductCRUDSOAPClient.class);
+//
+//	public ProductCRUDSOAPClient() throws Exception {
+//
+//
+//		/*
+//		 * create a client for the web service using ResteasyClientBuilder and ResteasyWebTarget
+//		 */
+//		proxy = null;
+//	}
+//
+//	public AbstractProduct createProduct(IndividualisedProductItem prod) {
+//		AbstractProduct created = proxy.createProduct(prod);
+//		// as a side-effect we set the id of the created product on the argument before returning
+//		prod.setId(created.getId());
+//		return created;
+//	}
+//
+//	public List<?> readAllProducts() {
+//		return proxy.readAllProducts();
+//	}
+//
+//	public AbstractProduct updateProduct(AbstractProduct update) {
+//		return proxy.updateProduct(update);
+//	}
+//
+//	public boolean deleteProduct(long id) {
+//		return proxy.deleteProduct(id);
+//	}
+//
+//	public AbstractProduct readProduct(long id) {
+//		return proxy.readProduct(id);
+//	}
+
+}
